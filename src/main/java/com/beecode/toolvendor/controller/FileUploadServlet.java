1d9ac8ebc154da68dbf0e41e4db077c36929511c
Luis Vespa <vespasoft@gmail.com>
2017-03-08 15:19:30 -0400

    arreglo en servicio UploadFile

--- src/main/java/com/beecode/toolvendor/controller/FileUploadServlet.java
+++ src/main/java/com/beecode/toolvendor/controller/FileUploadServlet.java
@@ -63,7 +63,7 @@ public class FileUploadServlet extends HttpServlet {
             //printEachPart(part, out);
             printJSONPart(part, out);
             String fileName = "products/" + getFileName(part);
-            //part.write(getFileName(part));
+            part.write(getFileName(part));
             s3service.uploadFile("toolvendor-files-bucket", fileName, 
                     new File(System.getenv("OPENSHIFT_DATA_DIR") + getFileName(part)));
         }

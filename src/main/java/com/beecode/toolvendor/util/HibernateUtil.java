0631254629508ac1f9cc8f886ff9b35b84d22f7b
Luis Vespa <vespasoft@gmail.com>
2017-07-24 19:46:35 -0500

    configuracion to openshift

--- src/main/java/com/beecode/toolvendor/util/HibernateUtil.java
+++ src/main/java/com/beecode/toolvendor/util/HibernateUtil.java
@@ -18,7 +18,7 @@ import org.hibernate.service.ServiceRegistry;
  */
 public class HibernateUtil {
 
-    public static final String CATALOG = "toolvendordev";       // LIVE
+    public static final String CATALOG = "toolvendorv1";       // LIVE
     //public static final String CATALOG = "toolvendor";        // LOCAL
     
     private static SessionFactory sessionFactory;

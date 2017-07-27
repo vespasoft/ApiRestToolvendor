36fe9e7b2703abe70bc16d69b733ae587020de45
Luis Vespa <vespasoft@gmail.com>
2017-07-27 00:28:20 -0500

    test pdf iText

--- src/main/java/com/beecode/toolvendor/service/PDFServiceImpl.java
+++ src/main/java/com/beecode/toolvendor/service/PDFServiceImpl.java
@@ -6,17 +6,27 @@
 package com.beecode.toolvendor.service;
 
 import com.itextpdf.text.Anchor;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Chapter;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.List;
+import com.itextpdf.text.ListItem;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Section;
 import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -51,6 +61,65 @@ public class PDFServiceImpl {
             document.add(new Paragraph("Some more text on the first page with different color and font type.", 
 
             FontFactory.getFont(FontFactory.COURIER, 14, Font.BOLD, new CMYKColor(0, 255, 0, 0))));
+            
+            // CHAPTER
+            Paragraph title1 = new Paragraph("Chapter 1", 
+            FontFactory.getFont(FontFactory.HELVETICA, 
+            18, Font.BOLDITALIC, new CMYKColor(0, 255, 255,17)));
+            Chapter chapter1 = new Chapter(title1, 1);
+            chapter1.setNumberDepth(0);
+            
+            
+            // SECTION
+            Paragraph title11 = new Paragraph("This is Section 1 in Chapter 1", 
+            FontFactory.getFont(FontFactory.HELVETICA, 16, Font.BOLD, 
+            new CMYKColor(0, 255, 255,17)));
+            Section section1 = chapter1.addSection(title11);
+            Paragraph someSectionText = new Paragraph("This text comes as part of section 1 of chapter 1.");
+            section1.add(someSectionText);
+            someSectionText = new Paragraph("Following is a 3 X 2 table.");
+            section1.add(someSectionText);
+            
+            // TABLE 
+            PdfPTable t = new PdfPTable(3);
+            t.setSpacingBefore(25);
+            t.setSpacingAfter(25);
+            PdfPCell c1 = new PdfPCell(new Phrase("Header1"));  
+            t.addCell(c1);
+            PdfPCell c2 = new PdfPCell(new Phrase("Header2"));
+            t.addCell(c2);
+            PdfPCell c3 = new PdfPCell(new Phrase("Header3"));
+            t.addCell(c3);
+            Image image2;
+            Image image3;
+            try {
+                image2 = Image.getInstance("https://s3.amazonaws.com/toolvendor-files-bucket/products/121123_1_8826_glm25181.jpg");
+                image2.scaleAbsolute(120f, 120f);
+                t.addCell(image2);
+                t.addCell("1.2");
+                image3 = Image.getInstance("https://s3.amazonaws.com/toolvendor-files-bucket/products/42e2ea226a2ef01117a087beafde2223363cd978.png");
+                image3.scaleAbsolute(120f, 120f);
+                t.addCell(image3);
+            } catch (BadElementException | IOException ex) {
+                Logger.getLogger(PDFServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            section1.add(t);
+            
+            // List Object
+            List l = new List(true, false, 10);
+            l.add(new ListItem("First item of list"));
+            l.add(new ListItem("Second item of list"));
+            section1.add(l);
+
+            // Image Object
+            try {
+                Image image = Image.getInstance("https://s3.amazonaws.com/toolvendor-files-bucket/products/e94bfb16ad3cefb62d1e0e83f355133fc4322b7e.png");
+                image.scaleAbsolute(120f, 120f);
+                section1.add(image);
+            } catch (BadElementException | IOException ex) {
+                Logger.getLogger(PDFServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            document.add(chapter1);
             
             document.close();
             
